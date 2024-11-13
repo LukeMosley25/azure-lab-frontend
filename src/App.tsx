f22354ebb5689504c15dfa5f0adee766e657c97e
Jeff Maxwell <jeff@tcnsolutions.com>
2024-11-12 22:35:25 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 //import Nav from './components/nav';
