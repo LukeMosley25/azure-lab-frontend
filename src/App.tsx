3eee2020e5789b7f1bdaa8757dee763a97ae5c13
Jeff Maxwell <jmaxwell@okcu.edu>
2024-11-19 21:32:49 -0600

    Add Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css';
 import Products from './components/products';
+import Nav from './components/nav';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Products />
     </div>
   );
